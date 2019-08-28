6464a7eb53ffd3c01701432e46696e11d4d62d06
adesupraptolaia <ades@alterra.id>
2019-08-28 19:36:37 +0700

    react-travis

---
+++ build/precache-manifest.b331697ec005153d7c06713ddf150000.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd07d6eb9b50863bea753ce28acca698",
+    "url": "/static/media/soldout.cd07d6eb.png"
+  },
+  {
+    "revision": "b598dd8f77adbbc2ebf5",
+    "url": "/static/css/main.da3ad47d.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ba9414471e5aaa94f890",
+    "url": "/static/js/2.ae12917d.chunk.js"
+  },
+  {
+    "revision": "fd44b07aaf120feee8adeda059faf6f0",
+    "url": "/static/media/bg-cpu.fd44b07a.jpg"
+  },
+  {
+    "revision": "6121232083996c53a92f60af62fff972",
+    "url": "/static/media/bg-pattern.61212320.svg"
+  },
+  {
+    "revision": "b598dd8f77adbbc2ebf5",
+    "url": "/static/js/main.a14eee0d.chunk.js"
+  },
+  {
+    "revision": "267eb51c2206f2a311f822a06c3f7463",
+    "url": "/static/media/slide-home-2.267eb51c.jpg"
+  },
+  {
+    "revision": "ac900a39b852581da9aeae094777cbd6",
+    "url": "/static/media/slide-home-1.ac900a39.jpg"
+  },
+  {
+    "revision": "79c412bc21ecf6e7e14c77a760338550",
+    "url": "/static/media/slide-home-3.79c412bc.jpg"
+  },
+  {
+    "revision": "144d4fce6154ca54b80fe1645e58412d",
+    "url": "/static/media/habis.144d4fce.png"
+  },
+  {
+    "revision": "0c795cb131fbe80d0de65bf958490ddc",
+    "url": "/index.html"
+  }
+];
--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-eval "$(ssh-agent -s)" &&
-ssh-add -k ~/.ssh/id_rsa &&
-cd /var/www/helloworld
-git pull
-
-source ~/.profile
-echo "$DOCKERHUB_PASS" | docker login --username $DOCKERHUB_USER --password-stdin
-docker stop helloworld
-docker rm helloworld
-docker rmi aprynur/helloworld
-docker run -d --name helloworld -p 5000:5000 aprynur/helloworld:latest
+sudo docker stop react-travis
+sudo docker rm react-travis
+sudo docker rmi adesupraptolaia/react-travis
+sudo docker run -d --name react-travis -p 3000:80 adesupraptolaia/react-travis:latest
